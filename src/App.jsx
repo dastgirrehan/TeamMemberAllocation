0fc3b160a571045dce175539554f040c36f18f2b
Ghulam Dastgir <ghulamdastgir04@gmail.com>
2022-11-10 21:17:06 +0000

    static function completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,56 @@
 import './App.css';
 import Header from './Header';
-import Content from './Content';
+import Employees from './Employees';
 import Footer from './Footer';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [ selectedTeam, setTeam ] = useState("TeamB")
+
+  const [employees, setEmployees] = useState([ 
+                {id: 1, fullName: "Employee1", designation: "Developer1", gender: "male", teamName: "TeamA"},
+                {id: 2, fullName: "Employee2", designation: "Developer2", gender: "female", teamName: "TeamA"},
+                {id: 3, fullName: "Employee3", designation: "Developer3", gender: "male", teamName: "TeamA"},
+                {id: 4, fullName: "Employee4", designation: "Developer4", gender: "male", teamName: "TeamB"},
+                {id: 5, fullName: "Employee5", designation: "Developer5", gender: "female", teamName: "TeamB"},
+                {id: 6, fullName: "Employee6", designation: "Developer6", gender: "female", teamName: "TeamB"},
+                {id: 7, fullName: "Employee7", designation: "Developer7", gender: "male", teamName: "TeamC"},
+                {id: 8, fullName: "Employee8", designation: "Developer8", gender: "female", teamName: "TeamC"},
+                {id: 9, fullName: "Employee9", designation: "Developer9", gender: "male", teamName: "TeamC"},
+                {id: 10, fullName: "Employee10", designation: "Developer10", gender: "female", teamName: "TeamD"},
+                {id: 11, fullName: "Employee11", designation: "Developer11", gender: "female", teamName: "TeamD"},
+                {id: 12, fullName: "Employee12", designation: "Developer12", gender: "male", teamName: "TeamD"},
+        ]);
+
+  function handleTeamSelectionChange(event){
+
+    console.log(event.target.value);
+    setTeam(event.target.value);
+  }
+
+  function handleEmployeeCardClick(event){
+    const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id)
+      ?(employee.teamName === selectedTeam)
+      ?{...employee, teamName: ""}:{...employee, teamName: selectedTeam}
+      :employee);
+
+    setEmployees(transformedEmployees);
+  }
+  
   return (
     <div>
-      <Header />
-      <Content />
+      <Header 
+        selectedTeam={selectedTeam}
+        teamMemberCount={employees.filter((employee) => employee.teamName === selectedTeam).length}
+        />
+      <Employees 
+        employees={employees}
+        selectedTeam={selectedTeam}
+        handleTeamSelectionChange={handleTeamSelectionChange}
+        handleEmployeeCardClick={handleEmployeeCardClick}
+        
+        />
       <Footer />
     </div>
   )
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,13 @@
 const Footer = () => {
+
+  var today = new Date();
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className="container">
+      <div className="row justify-content-center mt-4 mb-4">
+        <div className="col-8">
+          <h5>Team Allocation APP - {today.getFullYear()}</h5>
+        </div>
+      </div>
     </footer>
   )
 }
